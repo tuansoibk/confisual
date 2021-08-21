f3247c897fde760b703a35655c913f6c227bb38e
namnh97 <33318734+namnh97@users.noreply.github.com>
2021-08-21 13:53:52 +0700

    feat: implement visualiser. (#5)
    * feat: implement visualiser.
    * Use plantuml library to generate PNG image.
    
    Co-authored-by: Nam Hoài Nguyễn <nam.nguyen@adnovum.vn>

--- src/test/java/org/cp/confisual/nevisauth/integration/NevisAuthStateDiagramIntTest.java
+++ src/test/java/org/cp/confisual/nevisauth/integration/NevisAuthStateDiagramIntTest.java
@@ -1,36 +1,53 @@
 package org.cp.confisual.nevisauth.integration;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.cp.confisual.ParserException;
 import org.cp.confisual.TestUtils;
 import org.cp.confisual.nevisauth.Domain;
 import org.cp.confisual.nevisauth.Parser;
-import org.cp.confisual.nevisauth.Visualiser;
+import org.cp.confisual.nevisauth.NevisAuthVisualiser;
+import org.cp.confisual.VisualisationException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.cp.confisual.nevisauth.NevisAuthVisualiser.IMG_EXTENSION;
+import static org.junit.jupiter.api.Assertions.*;
 
 class NevisAuthStateDiagramIntTest {
 
-  private Visualiser underTest;
+  // support objects
+  private final Path destinationFolder = Paths.get(System.getProperty("user.dir") + "/build/libs");
+  private final File nevisAuthFile = TestUtils.getTestResourceFile("esauth4.xml");
+  private List<Domain> domains;
 
   @BeforeEach
-  void setUp() {
-    underTest = new Visualiser();
+  void setUp() throws ParserException {
+    domains = new Parser().parse(nevisAuthFile);
+    domains.forEach(domain -> {
+      try {
+        Files.deleteIfExists(destinationFolder.resolve(domain.getName() + IMG_EXTENSION));
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+      }
+    });
   }
 
   @Test
-  void canGenerateNevisAuthStateDiagram() throws ParserException {
+  void canGenerateNevisAuthStateDiagram() throws VisualisationException {
     // given
-    List<Domain> domains = new Parser().parse(TestUtils.getTestResourceFile("esauth4.xml"));
+    NevisAuthVisualiser visualiser = new NevisAuthVisualiser(destinationFolder);
 
     // when
-    underTest.visualise(domains);
+    visualiser.visualiseDomains(nevisAuthFile);
 
     // then
-    // assert visualisation result exists
-    fail("not yet implemented");
+    domains.forEach(domain -> assertTrue(Files.exists(destinationFolder.resolve(domain.getName() + IMG_EXTENSION))));
   }
 }
