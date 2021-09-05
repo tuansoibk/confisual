02ea8ba0c592a9d4364d6f75d32d4e0fb5d34808
Nam Hoài Nguyễn <nam.nguyen@adnovum.vn>
2021-09-05 10:13:47 +0700

    feat: create visualize controller. * Modify NevisAuthVisualiser to support both File and String.

--- confisual-be/src/main/java/org/cp/confisual/VisualizationController.java
+++ confisual-be/src/main/java/org/cp/confisual/VisualizationController.java
@@ -2,15 +2,38 @@ package org.cp.confisual;/*
  * Author : AdNovum Informatik AG
  */
 
+import java.util.List;
+
+import org.cp.confisual.nevisauth.NevisAuthConfigRequest;
+import org.cp.confisual.nevisauth.NevisAuthVisualiser;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class VisualizationController {
 		@GetMapping("/")
 		public String index() {
 				return "Greetings from Spring Boot!";
 		}
 
+		@PostMapping(path = "/visualise/nevisAuth")
+		public ResponseEntity<List<String>> visualizeNevisAuth(@RequestBody NevisAuthConfigRequest data) {
+				NevisAuthVisualiser visualiser= new NevisAuthVisualiser();
 
+				List<String> responseData = null;
+				try {
+						responseData = visualiser.visualiseDomains(data.getNevisAuthConfig());
+				}
+				catch (VisualisationException e) {
+						return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+				}
+
+				return new ResponseEntity<>(responseData, HttpStatus.OK);
+		}
 }
---
+++ confisual-be/src/main/java/org/cp/confisual/nevisauth/NevisAuthConfigRequest.java
@@ -0,0 +1,14 @@
+/*
+ * Author : AdNovum Informatik AG
+ */
+
+package org.cp.confisual.nevisauth;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class NevisAuthConfigRequest {
+		public String nevisAuthConfig;
+}
