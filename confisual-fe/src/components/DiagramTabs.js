34e45ba3c74e89c00f1bfcb57469a226a1df61d1
Nam Hoài Nguyễn <nam.nguyen@adnovum.vn>
2021-10-14 21:01:32 +0700

    feat: allow to search domain in UI * Create autocomplete field. * Fix proxy config.

--- confisual-fe/src/components/DiagramTabs.js
+++ confisual-fe/src/components/DiagramTabs.js
@@ -2,6 +2,8 @@ import Tabs from '@material-ui/core/Tabs';
 import React from 'react';
 import Tab from '@material-ui/core/Tab';
 import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
@@ -48,9 +50,14 @@ export default function DiagramTabs(props) {
     setValue(newValue);
   };
 
+  const handleSearchChange = (event, newValue) => {
+    const domain = diagrams.findIndex(diagram => diagram[0] === newValue);
+    setValue(domain);
+  };
+
   return (
     <Box sx={{ width: '80%' }}>
-      <Grid container direction="column" alignContent="center">
+      <Grid container alignContent="center" spacing={3}>
         <Grid item xs={8}>
           <Tabs
               classes={{root: { justifyContent: "center" }, scroller: { flexGrow: "0" }}}
@@ -70,28 +77,38 @@ export default function DiagramTabs(props) {
           }
           </Tabs>
         </Grid>
+        <Grid item xs={4}>
+          <Autocomplete
+              id="combo-box-search-domain"
+              options={diagrams.map(([domain, _]) => domain)}
+              renderInput={(params) => <TextField {...params} label="Domain" />}
+              onChange={handleSearchChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          {
+            diagrams.map(([_, diagram], idx) => (
+              <TabPanel
+                value={value}
+                index={idx}
+              >
+                <Box sx={{ border: '1px solid grey' }}>
+                  <TransformWrapper
+                    centerOnInit
+                    minScale={0.2}>
+                    <TransformComponent wrapperStyle={{margin: "auto", maxWidth: "100%"}}>
+                        <img
+                          alt="domain diagram"
+                          src={"data:image/png;base64," + diagram}
+                        />
+                    </TransformComponent>
+                  </TransformWrapper>
+                </Box>
+              </TabPanel>
+            ))
+          }
+        </Grid>
       </Grid>
-      {
-        diagrams.map(([_, diagram], idx) => (
-          <TabPanel
-            value={value}
-            index={idx}
-          >
-            <Box sx={{ border: '1px solid grey' }}>
-              <TransformWrapper
-                centerOnInit
-                minScale={0.2}>
-                <TransformComponent wrapperStyle={{margin: "auto", maxWidth: "100%"}}>
-                    <img
-                      alt="domain diagram"
-                      src={"data:image/png;base64," + diagram}
-                    />
-                </TransformComponent>
-              </TransformWrapper>
-            </Box>
-          </TabPanel>
-        ))
-      }
     </Box>
   );
 }
